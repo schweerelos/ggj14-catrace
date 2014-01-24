13a8ca40b53f8b30d30ca9b6f2fdfaf4acc07a12
andreas.loow@gmail.com <andreas.loow@gmail.com@bd02273f-8a89-29e2-0336-7233999f62d2>
2014-01-24 09:37:19 +0000

    Started to parse keyboard input. Added a basic Player class that incorporates player movement in the left and right direction.
    Updated camera view to point in the right direction, the Z plane was inverted.

---
+++ trunk/CatGame/CatGame/Player.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace CatGame
+{
+    class Player
+    {
+        protected int lane;
+        Model avatar;
+
+        public Player(Model avatar){
+            this.avatar = avatar;
+        }
+
+        public Player(int number)
+        {
+            lane = 0;
+        }
+
+        public void moveLeft()
+        {
+            if (lane >= -2)
+                lane--;
+        }
+
+        public void moveRight()
+        {
+            if (lane <= 2)
+                lane++;
+        }
+
+        public void draw(Matrix world, Matrix view, Matrix projection)
+        {
+            world *= Matrix.CreateTranslation(lane, 0, 0);
+            avatar.Draw(world, view, projection);
+        }
+
+    }
+}
