172506be0a6e2cc27bcf474ca93e0ceb33ef211b
andrea.schweer@gmail.com <andrea.schweer@gmail.com@bd02273f-8a89-29e2-0336-7233999f62d2>
2014-01-24 21:20:04 +0000

    Obstacles appear regularly. They don't change state/position at all yet.

--- trunk/CatGame/CatGame/Obstacle.cs
+++ trunk/CatGame/CatGame/Obstacle.cs
@@ -2,10 +2,64 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
 
 namespace CatGame
 {
     class Obstacle
     {
+        private Model avatar;
+        private int lane;
+        private int size; // -1 = small; 0 = normal; 1 = big; 2 = huge
+        private float distanceTravelled;
+
+        public Obstacle(Model avatar, int lane)
+        {
+            this.avatar = avatar;
+            this.lane = lane;
+            size = 0; // "normal" size initially
+            distanceTravelled = -100; // TODO use sensible value
+        }
+
+        public void moveLeft()
+        {
+            // TODO take size of obstacle into account
+            if (lane >= 1)
+                lane--;
+        }
+
+        public void moveRight()
+        {
+            // TODO take size of obstacle into account
+            if (lane <= 5)
+                lane++;
+        }
+
+        public void increaseSize()
+        {
+            // TODO what if obstacle is already on the edge of the ramp?
+            if (size <= 1)
+                size++;
+        }
+
+        public void decreaseSize()
+        {
+            if (size >= 0)
+                size--;
+        }
+
+        public void progress(float amount)
+        {
+            distanceTravelled += amount;
+        }
+
+        public void draw(Matrix view, Matrix projection)
+        {
+            // TODO take size into account
+            Matrix world = Matrix.Identity;
+            world *= Matrix.CreateTranslation(lane, 0, 0);
+            avatar.Draw(world, view, projection);
+        }
     }
 }
