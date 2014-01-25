5f88ea3ef669a414021c90966d148df72bc8163f
andrea.schweer@gmail.com <andrea.schweer@gmail.com@bd02273f-8a89-29e2-0336-7233999f62d2>
2014-01-25 04:24:43 +0000

    Cap new obstacle rate increase

--- CatGame/CatGame/Game1.cs
+++ CatGame/CatGame/Game1.cs
@@ -175,7 +175,8 @@ namespace CatGame
             {
                 obstacles.Add(new Obstacle(randomSource.Next(0,7)));
                 elapsedSinceLastObstacle = 0;
-                newObstacleThreshold *= 0.995;
+                if (newObstacleThreshold > 700)
+                    newObstacleThreshold *= 0.995;
                 //Console.WriteLine("obstacle threshold: " + newObstacleThreshold);
             }
 
