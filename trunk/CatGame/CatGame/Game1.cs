8448b5b42de3bd7fed97d14d19d17e73e89d4771
andrea.schweer@gmail.com <andrea.schweer@gmail.com@bd02273f-8a89-29e2-0336-7233999f62d2>
2014-01-25 06:17:27 +0000

    Better cap for max new obstacles rate?

--- trunk/CatGame/CatGame/Game1.cs
+++ trunk/CatGame/CatGame/Game1.cs
@@ -176,7 +176,7 @@ namespace CatGame
             {
                 obstacles.Add(new Obstacle(randomSource.Next(0,7)));
                 elapsedSinceLastObstacle = 0;
-                if (newObstacleThreshold > 700)
+                if (newObstacleThreshold > 900)
                     newObstacleThreshold *= 0.995;
                 //Console.WriteLine("obstacle threshold: " + newObstacleThreshold);
             }
